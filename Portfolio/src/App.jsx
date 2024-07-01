0ba06fbd809a1380149c1553969c5dad8e64b463
Evan Tardiff <10evantardiff@gmail.com>
2024-07-01 01:21:16 -0700

    did About section

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import styles from './App.module.css';
 import { Navbar } from './componets/Navbar/Navbar';
 import { Hero } from './componets/Hero/Hero';
+import { About } from './componets/About/About';
 
 function App() {
   return (<div className={styles.App}>
     <Navbar />
     <Hero />
+    <About />
   </div>);
 }
 
