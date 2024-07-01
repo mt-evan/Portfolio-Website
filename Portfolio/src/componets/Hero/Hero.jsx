6f8fd787532695f46abda164a7fe81d34f24731d
Evan Tardiff <10evantardiff@gmail.com>
2024-06-30 22:00:06 -0700

    did Hero section

---
+++ Portfolio/src/componets/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
+
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I'm Evan</h1>
+                <p className={styles.description}>I'm a full stack developer with 5 years of experiece using React and NodeJS. Reach out if you'd like to learn more.</p>
+                <a href="mailto:myemail@.com" className={styles.contactBtn}>Contact Me</a>
+            </div>
+            <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
+            <div className={styles.topBlur}/>
+            <div className={styles.bottomBlur}/>
+        </section>
+    );
+};
