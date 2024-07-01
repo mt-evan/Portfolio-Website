57c2504cfb49ea39842444fe473e89347e17f262
Evan Tardiff <10evantardiff@gmail.com>
2024-07-01 02:58:26 -0700

    did experience section

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,12 +2,14 @@ import styles from './App.module.css';
 import { Navbar } from './componets/Navbar/Navbar';
 import { Hero } from './componets/Hero/Hero';
 import { About } from './componets/About/About';
+import { Experience } from './componets/Experience/Experience';
 
 function App() {
   return (<div className={styles.App}>
     <Navbar />
     <Hero />
     <About />
+    <Experience />
   </div>);
 }
 
