9b6eabcf6f3cb89f70c3fda9b56db0fd98feab23
Evan Tardiff <10evantardiff@gmail.com>
2024-07-04 15:54:27 -0700

    changed images in About section

--- Portfolio/src/componets/About/About.jsx
+++ Portfolio/src/componets/About/About.jsx
@@ -9,24 +9,24 @@ export const About = () => {
         <section className={styles.container} id="about">
             <h2 className={styles.title}>ABOUT</h2>
             <div className={styles.content}>
-                <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" className={styles.aboutImage}></img>
+                <img src={getImageUrl("about/sdsu.png")} alt="Me sitting with a laptop" className={styles.aboutImage}></img>
                 <ul className={styles.aboutItems}>
                     <li className={styles.aboutItem}>
-                        <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon"></img>
+                        <img src={getImageUrl("about/education.png")} alt="Cursor icon"></img>
                         <div className={styles.aboutItemText}>
                             <h3>Education</h3>
                             <p>I'm currently enrolled in my third year at SDSU, and I am set to graduate either in December 2025 or May 2026. I currently have a 3.86 GPA.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
-                        <img src={getImageUrl("about/serverIcon.png")} alt="Server icon"></img>
+                        <img src={getImageUrl("about/extra.png")} alt="Server icon"></img>
                         <div className={styles.aboutItemText}>
                             <h3>Extracurriculars</h3>
                             <p>I am involved in the MESA program since August 2022 as well as the AI club since August 2023 here at SDSU.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
-                        <img src={getImageUrl("about/uiIcon.png")} alt="UI icon"></img>
+                        <img src={getImageUrl("about/location.png")} alt="UI icon"></img>
                         <div className={styles.aboutItemText}>
                             <h3>Location</h3>
                             <p>I live in Rowland Heights in LA County, California, but I am open to relocating or working remote.</p>
