32247f7d2fedbff03ccbe2282ec231f3e3faba6c
mt-evan <10evantardiff@gmail.com>
2024-09-02 17:29:02 -0700

    updated my GPA

--- Portfolio/src/componets/About/About.jsx
+++ Portfolio/src/componets/About/About.jsx
@@ -15,7 +15,7 @@ export const About = () => {
                         <img src={getImageUrl("about/education.png")} alt="Cursor icon"></img>
                         <div className={styles.aboutItemText}>
                             <h3>Education</h3>
-                            <p>I'm currently enrolled in my third year at SDSU, and I am set to graduate either in December 2025 or May 2026. I currently have a 3.86 GPA.</p>
+                            <p>I'm currently enrolled in my third year at SDSU, and I am set to graduate either in December 2025 or May 2026. I currently have a 3.87 GPA.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
