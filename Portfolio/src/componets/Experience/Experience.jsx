6f87ce0649142c374a75302d774d34e61969bbe8
mt-evan <10evantardiff@gmail.com>
2024-09-02 17:20:15 -0700

    changed image of google to sdsu

--- Portfolio/src/componets/Experience/Experience.jsx
+++ Portfolio/src/componets/Experience/Experience.jsx
@@ -13,7 +13,7 @@ export const Experience = () => {
                     {
                         history.map((historyItem, id) => {
                             return (<li key={id} className={styles.historyItem}>
-                                <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`}></img>
+                                <img className={styles.historyItemImage} src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`}></img>
                                 <div className={styles.historyItemDetails}>
                                     <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                                     <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
