2c5306b8b48470b7a75d9fd119690691c7d43950
mt-evan <10evantardiff@gmail.com>
2024-12-13 15:11:56 -0800

    Updated Hero section

--- Portfolio/src/componets/Hero/Hero.jsx
+++ Portfolio/src/componets/Hero/Hero.jsx
@@ -10,7 +10,8 @@ export const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Evan</h1>
-                <p className={styles.description}>I'm a current 3rd year at San Diego State University majoring in Computer Science with a minor in Mathematics. I am interested in Software Engineering, Web Development, and Data Analysis. Feel free to reach out.</p>
+                <p className={styles.description}>I'm a current 3rd year at San Diego State University majoring in Computer Science with a minor in Mathematics. 
+                I will be with Lutron Electronics in the upcoming summer of 2025 as a Software Engineering Intern in their Philadelphia office. Feel free to reach out.</p>
                 <a href="mailto:10evantardiff@gmail.com" className={styles.contactBtn}>Contact Me</a>
             </div>
             <img src={getImageUrl("hero/evanSI.png")} alt="Image of me" className={styles.heroImg}/>
