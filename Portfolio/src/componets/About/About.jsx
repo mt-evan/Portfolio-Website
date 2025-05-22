c7b1ac28113f1cd57e66ecf41e4963d39cbc6713
mt-evan <10evantardiff@gmail.com>
2025-05-22 00:57:01 -0700

    Updated GPA and Education section wording

--- Portfolio/src/componets/About/About.jsx
+++ Portfolio/src/componets/About/About.jsx
@@ -15,15 +15,15 @@ export const About = () => {
                         <img src={getImageUrl("about/education.png")} alt="Cursor icon"></img>
                         <div className={styles.aboutItemText}>
                             <h3>Education</h3>
-                            <p>I'm currently enrolled in my third year at SDSU, and I am set to graduate in December 2025. I currently have a 3.88 GPA.</p>
+                            <p>I am currently in my fourth year at San Diego State University pursuing a Bachelor's degree in Computer Science with a minor in Mathematics, and I am expected to graduate in May 2026. I currently have a 3.90 GPA.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/extra.png")} alt="Server icon"></img>
                         <div className={styles.aboutItemText}>
                             <h3>Extracurriculars</h3>
-                            <p>I have been involved in the MESA program since August 2022 and participated in the AI club from August 2023 to May 2024 here at SDSU. 
-                            I have also joined the AI4Business research lab in December 2024 where we use machine learning to solve real-world problems and for research projects.</p>
+                            <p>Since August 2022, I have been involved in the MESA program at SDSU. I participated in the AI Club from August 2023 to May 2024. 
+                                In December 2024, I joined the AI4Business research lab, where we use machine learning to solve real-world problems and conduct research projects.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
